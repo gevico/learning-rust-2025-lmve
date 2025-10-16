49e774c9378487bba80669bc1f9efc344734c7b3
lmve <1831232566@qq.com>
2025-10-16 15:08:57 +0000

    do some exercise. 30 - 50
    Signed-off-by: lmve <1831232566@qq.com>

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,8 +5,21 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+/*
+    字符串字面量适用于：
+    程序中固定的文本
+    不需要修改的字符串
+    性能要求高的场景
+    &str 适用于：
+    函数参数（通用性好）
+    字符串处理中的临时视图
+    不需要拥有数据所有权的情况
+    String 适用于：
+    需要修改字符串内容
+    运行时构建字符串
+    需要拥有数据所有权
+ */
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
     if is_a_color_word(word) {
@@ -16,6 +29,6 @@ fn main() {
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
+fn is_a_color_word(attempt: String) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
